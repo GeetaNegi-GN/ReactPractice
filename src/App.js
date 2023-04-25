2b11c059b9ddceaab562e2c8ceb9e3df753c4478
Geeta Negi <geetanegi550@gmail.com>
2023-04-25 22:58:06 +0530

    Props,Proptypes,State and handling exvent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
 
 
 function App() {
   return (
     <>
-      <Navbar/>
+      <Navbar title = "TextUtils" aboutText = "About Us"/>
+      <div className="container my-3">
+        <TextForm heading = "Enter the text to analyze below"/>
+        </div>
     </>
   );
 }
