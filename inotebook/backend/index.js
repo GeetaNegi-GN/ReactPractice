a5f3b5ea8790f040da69664ad43660dbb9396331
Geeta Negi <geetanegi550@gmail.com>
2023-05-08 19:29:59 +0530

    storing data and data validation

--- inotebook/backend/index.js
+++ inotebook/backend/index.js
@@ -4,6 +4,8 @@ const mongoose = require('./db.js');
 const express = require('express')
 const app = express()
 const port = 3000
+
+app.use(express.json())
 //Available routes
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -1,14 +1,30 @@
 
 const express = require('express');
+const User = require('../models/User');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
 
-router.get('/', (req,res)=>{
-    obj = {
-        a: 'thios',
-        number : 34
-    }
-    res.json(obj)
-})
 
+//Create a user using : Post "/api/auth/". Dosen't require auth
+router.post('/',[
+body('name').isLength({min:3}),
+    body('email').isEmail(),
+    body('password').isLength({min:5}),
+    
+] ,   (req, res) => {
+const result = validationResult(req);
+if (!result.isEmpty()) {
+  return res.status(400).json({result: result.array()});
+}
+User.create({
+    name: req.body.name,
+    password: req.body.password,
+    email: req.body.email,
+
+}).then(user => res.json(user))
+.catch(err => {console.log(err)
+    res.json({error: 'Please enter a unique value for email'})})
+
+});
 module.exports = router
