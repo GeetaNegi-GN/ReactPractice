9f155934e33aaa097c8ca861ae55bd3b2e0215d2
Geeta Negi <geetanegi550@gmail.com>
2023-05-08 18:09:13 +0530

    routes &  schema

--- inotebook/backend/index.js
+++ inotebook/backend/index.js
@@ -4,10 +4,10 @@ const mongoose = require('./db.js');
 const express = require('express')
 const app = express()
 const port = 3000
+//Available routes
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
 
-app.get('/', (req, res) => {
-  res.send('Hello!')
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
